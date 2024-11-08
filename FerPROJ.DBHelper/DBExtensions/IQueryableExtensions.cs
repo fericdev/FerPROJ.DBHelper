9008bec7d47f5f75478cba431aa6e8e853551727
fericqt <feric08c@gmail.com>
2024-11-08 09:06:24 +0800

    added iqueryable extension

---
+++ FerPROJ.DBHelper/DBExtensions/IQueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerPROJ.DBHelper.DBExtensions {
+    public static class IQueryableExtensions {
+        public static async Task SaveAndCommitAsync<TEntity>(
+             this DbContext context,
+             TEntity entity)
+             where TEntity : class {
+
+            context.Set<TEntity>().Add(entity);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
