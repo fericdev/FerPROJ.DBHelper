2c2815145df26bcbfbb013f3fc851a5db9550fdc
Feric Decenan <feric.decenan.git@gmail.com>
2025-08-27 11:14:42 +0800

    Fix

--- FerPROJ.DBHelper/Base/BaseEntity.cs
+++ FerPROJ.DBHelper/Base/BaseEntity.cs
@@ -11,11 +11,11 @@ namespace FerPROJ.DBHelper.Base {
         public Guid Id { get; set; }
         public string FormId { get; set; }
         public string Name { get; set; }
-        public DateTime DateCreated { get; set; }
+        public DateTime? DateCreated { get; set; } = null;
         public DateTime? DateModified { get; set; } = null;
         public DateTime? DateDeleted { get; set; } = null;
         public string CreatedBy { get; set; }
-        public string ModifiedBy { get; set; } = string.Empty;
+        public string ModifiedBy { get; set; } = null;
         public string Status { get; set; }
     }
 }
