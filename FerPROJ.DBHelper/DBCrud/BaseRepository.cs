ccd1a1dfb396f9f446a6517ea36d23e6cda732ad
Feric Decenan <feric.decenan.git@gmail.com>
2026-04-21 10:37:16 +0800

    yes

--- FerPROJ.DBHelper/DBCrud/BaseRepository.cs
+++ FerPROJ.DBHelper/DBCrud/BaseRepository.cs
@@ -845,22 +845,7 @@ namespace FerPROJ.DBHelper.DBCrud {
 
         #region Remarks
         public async Task<bool> SaveRemarksDTOAsync(RemarksModel myDTO) {
-            if (myDTO == null)
-                throw new ArgumentNullException($"{nameof(myDTO)} is null!");
-
-            if (!myDTO.DataValidation()) {
-                var sb = new StringBuilder();
-                if (!string.IsNullOrEmpty(myDTO.Error))
-                    sb.AppendLine("Error 1: " + myDTO.Error);
-                if (!string.IsNullOrEmpty(myDTO.ErrorMessage))
-                    sb.AppendLine("Error 2: " + myDTO.ErrorMessage);
-                if (myDTO.ErrorMessages.Length > 0)
-                    sb.AppendLine("Error 3: " + myDTO.ErrorMessages.ToString());
-                throw new ArgumentException(sb.ToString());
-            }
-
-            if (!myDTO.Success)
-                throw new ArgumentException(myDTO.Error);
+            myDTO.DataValidationResult();
 
             if (CDialogManager.Ask("Are you sure to save remarks?", "Confirmation")) {
                 await SaveRemarksDataAsync(myDTO);
