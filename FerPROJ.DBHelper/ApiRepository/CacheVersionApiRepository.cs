4ce7b0cab2bdd45563ea6a25944a9ea75c9eec3e
Feric Decenan <feric.decenan.git@gmail.com>
2026-05-15 09:07:17 +0800

    yes

---
+++ FerPROJ.DBHelper/ApiRepository/CacheVersionApiRepository.cs
@@ -0,0 +1,15 @@
+﻿using FerPROJ.DBHelper.DBCrud;
+using FerPROJ.DBHelper.Entity.Cache;
+using FerPROJ.Design.FormModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerPROJ.DBHelper.ApiRepository {
+    public class CacheVersionApiRepository : BaseApiRepository<CacheVersionModel, CacheVersion, Guid> {
+        public CacheVersionApiRepository() : base("CacheVersionApiRepository.php") {
+        }
+    }
+}
