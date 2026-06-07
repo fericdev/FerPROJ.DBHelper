09ba1512c36bc6f1a8fe8f59583e30ef1e810df9
Feric Decenan <feric.decenan.git@gmail.com>
2026-06-07 19:25:31 +0800

    adjust

---
+++ FerPROJ.DBHelper/Repository/CompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using FerPROJ.DBHelper.DBCrud;
+using FerPROJ.DBHelper.Entity.Companies;
+using FerPROJ.Design.FormModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerPROJ.DBHelper.Repository {
+    public class CompanyRepository : BaseRepository<BaseDbContext, SystemCompanyModel, SystemCompany, Guid> {
+    }
+}
