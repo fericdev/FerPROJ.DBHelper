e50b1f792aea67702d60fda1893c9377ebe1d1e7
Feric Decenan <feric.decenan.git@gmail.com>
2026-06-07 19:23:41 +0800

    company

---
+++ FerPROJ.DBHelper/Entity/Companies/SystemCompany.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerPROJ.DBHelper.Entity.Companies {
+    public class SystemCompany : BaseEntity {
+        [StringLength(int.MaxValue)]
+        public string CompanyLogoUrl { get; set; }
+        public string CompanyEmail { get; set; }
+        public string CompanyContactNo { get; set; }
+        public string CompanyAddress { get; set; }
+        public bool CompanyEnabled { get; set; }
+    }
+}
---
+++ FerPROJ.DBHelper/Entity/Companies/SystemCompanyMigration.cs
@@ -0,0 +1,20 @@
+﻿using FerPROJ.DBHelper.DBCrud;
+using FerPROJ.DBHelper.Helper;
+using FerPROJ.Design.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerPROJ.DBHelper.Entity.Companies {
+    public class SystemCompanyMigration : IDbContextMigration<BaseDbContext> {
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+
+        public async Task RunMigrationAsync(BaseDbContext dbContext) {
+            await DbContextHelper.CreateOrUpdateTableOfEntityAsync<SystemCompany>(dbContext);
+        }
+    }
+}
